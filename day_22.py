cd2140cca31940d75c66b8783be411036b5d4860
paul-lawrence <paul@dactl.com>
2022-12-22 11:39:26 -0500

    daf

--- day_22.py
+++ day_22.py
@@ -1,8 +1,16 @@
 import ast 
 import re
+import numpy as np
 
 def part_1(nums):
 	return -1
+	
+	
+def getwrap(A):
+	C=[]
+	C.append(next((i for i, v in enumerate(A) if v != 0), -1))
+	C.append(len(A)-1-next((i for i, v in enumerate(reversed(A)) if v != 0), -1))
+	return C
 	
 def main():
 	grid=[list(d.replace('\n','')) for d in open('day_22.txt','r').readlines()]
@@ -10,14 +18,18 @@ def main():
 	d={" ":0,'.':1,'#':-1}
 	path = ''.join(map(str, grid.pop()))
 	grid.pop()
-	grid=[[d[x] for x in y] for y in grid]
+	grid=np.array([[d[x] for x in y] for y in grid])
 	print(grid)
 	#print(path)
 	nums=[int(n) for n in map(int, re.findall(r'\d+', path))]
-	print(nums)
-	print(next((i for i, v in enumerate(grid[0]) if v != 0), -1))
-	print(grid[0])
-	print(len(grid[0])-1-next((i for i, v in enumerate(reversed(grid[0])) if v != 0), -1))
+	#print(nums)
+	#for A in grid:
+	#	print(getwrap(A))
+	#print(grid[-1][5])
+	print(grid[:,0])
+	for i in range(len(grid[0])):
+		print(getwrap(grid[i:i+1]), grid[0][i], grid[-1][i])
+	print(len(grid[0]))
 	#nums=[int(line.strip()) for line in open('day_20.txt', 'r').readlines()]
 	#print([n.value for n in nums])
 	#test=[1,2,-3,3,-2,0,4]
@@ -26,6 +38,5 @@ def main():
 	#print(part_2(test))
 	#cheat=[int(x.strip()) for x in open('day_20.txt','r').readlines()]
 	#ch(nums)
-	
 	
 main()
